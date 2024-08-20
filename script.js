32e3366d66a08ea342822c109bd2b1078fd8f468
elvis <nacanaboelvis7@gmail.com>
2024-08-20 15:44:54 +0000

    grang ecrant ok

--- script.js
+++ script.js
@@ -82,10 +82,14 @@ async function fetchData(){
         const resultat=await fetch(url1)
         const donnee=await resultat.json()
         const genres=donnee.genres
+        const icns=["icons8-action-96.png","icons8-boussole-50.png","icons8-animation-50 (1).png","icons8-comédie-50.png","icons8-piratage-informatique-50.png","icons8-documentaire-50.png","icons8-drame-50.png","icons8-famille-homme-femme-50.png","icons8-fantaisie-50.png","icons8-historique-des-commandes-50.png","icons8-pennywise-50.png","icons8-musique-50.png","icons8-troisième-oeil-symbole-50.png","icons8-rendez-vous-galant-homme-femme-50.png","icons8-robot-3-50.png","icons8-télévision-50.png","icons8-thriller-50.png","icons8-réfugié-de-guerre-50.png","icons8-occidental-50.png",]
         for(let i=0;i<genres.length;i++){
             let dive=document.createElement("div")
                 dive.classList.add("item")
                 dive.textContent=genres[i].name
+            let icons=document.createElement("img") 
+                icons.src=`icon/${icns[i]}` 
+                dive.appendChild(icons)  
             categorieContent.appendChild(dive)   
         }
           // affichage des films
@@ -138,18 +142,22 @@ let texte=document.getElementById("titre")
                                             // autre contenu
                                 const divImg=document.createElement("div")   
                                     divImg.classList.add("imgdiv")
+                                    divImg.classList.add("petiEcrantOnly")
                                     divImg.style.backgroundImage=`url(https://image.tmdb.org/t/p/w500${urlImage})`
+                             
                                             // indicatif de nouveauter
                                     if(differenceDeMoi<=7){
                                         const news=document.createElement("div") 
                                         news.textContent="NEW" 
                                         news.classList.add("new")
+                                        news.classList.add("petiEcrantOnly")
                                         divImg.appendChild(news) 
                                     } 
                                         div.appendChild(divImg)
                                           // titre
                                 const divtitr = document.createElement("div") 
                                     divtitr.classList.add("titre")
+                                    divtitr.classList.add("petiEcrantOnly")
                                 const para=document.createElement("p")
                                     divtitr.appendChild(para)
                                     para.textContent=titre
@@ -161,22 +169,116 @@ let texte=document.getElementById("titre")
                             resize()
 
                             let cards=document.querySelectorAll(".card")
-                            console.log(cards)
+                            let aCacher=document.querySelectorAll(".petiEcrantOnly")                 
+                            // console.log(cards)
                             for(let i=0;i<cards.length;i++){
+                                let urlImage=Alldonnee[i].backdrop_path
+                                let urlImage2=Alldonnee[i].poster_path  
                                 cards[i].addEventListener("click",()=>{
                                     const largeur=cards[i].offsetWidth
                                     if(largeur>250){
                                         // quand la div prend tout l'ecrant
                                         console.log("nous somme sur grand ecrant")
-                                        console.log(Alldonnee)
-                                        
+                                        aCacher.forEach(element=>element.classList.add("invisible"))
+                                        cards[i].style.backgroundImage=`url(https://image.tmdb.org/t/p/w500${urlImage})`
+                                                    // l'effet flou
+                                        const filter=document.createElement("div")
+                                                filter.classList.add("filter")
+                                                filter.classList.add("grandEcrantOnly")
+                                                    // container du center
+                                        const center=document.createElement("div")
+                                                center.classList.add("center")
+                                                center.classList.add("grandEcrantOnly")
+                                                filter.appendChild(center)  
+                                        const imtext=document.createElement("div")
+                                                imtext.classList.add("imtext")  
+                                        const im=document.createElement("div")
+                                                im.classList.add("im")
+                                        const image=document.createElement("div")
+                                                image.style.backgroundImage=`url(https://image.tmdb.org/t/p/w500${urlImage2})`
+                                                im.appendChild(image)        
+                                        const text=document.createElement("div") 
+                                                text.classList.add("text") 
+                                        const tr=document.createElement("h2")
+                                                tr.textContent=Alldonnee[i].title 
+                                        const p1=document.createElement("p")
+                                                p1.textContent=Alldonnee[i].overview 
+                                                text.appendChild(tr)   
+                                                text.appendChild(p1)                                              
+                                        const autreIfo=document.createElement("div")
+                                                autreIfo.classList.add("autreinfo")
+                                        const canva=document.createElement("div") 
+                                                canva.classList.add("canva")
+                                        const date=document.createElement("div") 
+                                                date.classList.add("date")
+                                        const p5=document.createElement("p")
+                                        const p6=document.createElement("p")
+                                                p5.textContent=`Date de sortie: ${Alldonnee[i].release_date}`
+                                                p6.textContent=`Nombre de votes: ${Alldonnee[i].vote_count}` 
+                                                date.appendChild(p5) 
+                                                date.appendChild(p6)       
+                                        const canvas=document.createElement("canvas")
+                                        canva.appendChild(canvas)   
+                                                autreIfo.appendChild(canva)
+                                                autreIfo.appendChild(date)   
+                                                im.classList.add("image")
+                                                imtext.appendChild(im)
+                                                imtext.appendChild(text)
+                                                center.appendChild(imtext)
+                                                center.appendChild(autreIfo)
+                                                text.classList.add("text")
+                                                cards[i].appendChild(filter)
+
+                                                       // popularity
+
+                                                canvas.classList.add("myRingChart")
+                                                const ctx = canvas.getContext('2d');
+                                                const myRingChart = new Chart(ctx, {
+                                                    type: 'doughnut', // Utilisez 'doughnut' pour un graphique en anneau
+                                                    data: {
+                                                        labels: ["popularity"],
+                                                        datasets: [{
+                                                            label: 'My Dataset',
+                                                            data: [Alldonnee[i].popularity,100],
+                                                            backgroundColor: [
+                                                                ' rgb(255, 0, 111)',
+                                                                'rgba(54, 162, 235, 0.2)'
+                                                            ],
+                                                            borderColor: [
+                                                                'rgba(255, 99, 132, 1)',
+                                                                'rgba(54, 162, 235, 1)'
+                                                            ],
+                                                            borderWidth: 1
+                                                        }]
+                                                    },
+                                                    options: {
+                                                        cutout: '60%', // Détermine la taille de l'anneau
+                                                        plugins: {
+                                                            legend: {
+                                                                position: 'bottom',
+                                                            },
+                                                            tooltip: {
+                                                                callbacks: {
+                                                                    label: function(tooltipItem) {
+                                                                        return tooltipItem.label + ': ' + tooltipItem.raw;
+                                                                    }
+                                                                }
+                                                            }
+                                                        }
+                                                    }
+                                                });         
+                                                
                                     }
                                         // quand la dive est petite 
+                                        let elementAcacher=document.querySelectorAll(".grandEcrantOnly")
+                                        // let image=querySelector("")
                                     if(largeur<=250){
                                             console.log("nous somme sur patit ecrant")
-                                            for(let i=0;i<Alldonnee.length; i++){
-                                                let name=Alldonnee[i].original_title
-                                            }
+                                            console.log(Alldonnee)
+                                            cards[i].style.backgroundImage=""
+                                            aCacher.forEach(element=>element.classList.remove("invisible"))
+                                            elementAcacher.forEach(element=>element.classList.add("invisible"))
+                                            console.log(elementAcacher)
                                     }
                                 })
                             }
